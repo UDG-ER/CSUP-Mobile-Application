0b1948b0f8f626fa04c88848b852f4b0da248207
BrankoJovovic <brankojovovic97@gmail.com>
2019-12-13 01:39:54 +0100

    Commit

---
+++ app/src/main/java/com/example/stomatoloskaordinacija_aplikacija/Zaposleni.java
@@ -0,0 +1,13 @@
+package com.example.stomatoloskaordinacija_aplikacija;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Zaposleni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zaposleni);
+    }
+}
